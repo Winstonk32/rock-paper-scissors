869d483251ed2802f3dee03e60f4e40a8be3ad75
Winstonk32 <winstonkigonya4@gmail.com>
2024-09-29 21:19:57 +0300

    java

---
+++ r.js
@@ -0,0 +1,29 @@
+const choices = ["rock", "paper", "scissors"];
+const playDisplay = document.getElementById('playDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+
+function play(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (playerChoice === computerChoice) {
+        result = "It's a tie";
+    } else {
+        switch(playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "You Win" : "You Lose";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "You Win" : "You Lose";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "You Win" : "You Lose";
+                break;
+        }
+    }
+
+    playDisplay.textContent = 'Player: ' + playerChoice;
+    computerDisplay.textContent = 'Computer: ' + computerChoice;
+    resultDisplay.textContent = result;
+}
